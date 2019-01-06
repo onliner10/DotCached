43176561a1f4b339bee83f797e1923a9f3069051
onliner10 <mateuszurban2@gmail.com>
2019-01-06 22:11:27 +0100

    Fixed unit tests

---
+++ DotCached.Core.UnitTests/DotCacheTests.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DotCached.Core.Infrastructure;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace DotCached.Core.UnitTests
+{
+    public class LazyTtlCacheTests
+    {
+        private const string DummyKey = "DummyKey";
+
+        private readonly Mock<IValueFactory<string, string>> _dummyValueFactory;
+
+        public LazyTtlCacheTests()
+        {
+            var dummyValueFactory = new Mock<IValueFactory<string, string>>();
+            dummyValueFactory.Setup(x => x.Get(DummyKey)).ReturnsAsync(DummyKey);
+            _dummyValueFactory = dummyValueFactory;
+        }
+
+        public static IEnumerable<object[]> ThreadCounts =>
+            Enumerable.Range(1, 30).Cast<object>().Select(x => new[] {x});
+
+        private TimeProvider CreateIncrementalTimeProvider()
+        {
+            var callCounter = 0;
+
+            DateTimeOffset DummyProvider()
+            {
+                var res = DateTimeOffset.UnixEpoch.Add(callCounter * TimeSpan.FromMinutes(1));
+                Interlocked.Increment(ref callCounter);
+                return res;
+            }
+
+            return DummyProvider;
+        }
+
+        [Theory]
+        [MemberData(nameof(ThreadCounts))]
+        public void GetOrNull_MultipleThreads_ValueFactoryIsCalledExatlyOnce(int threadCount)
+        {
+            var timeProvider = CreateIncrementalTimeProvider();
+
+            var invalidationStrategyMock = new Mock<IInvalidationStrategy<string>>();
+            invalidationStrategyMock
+                .Setup(x => x.ShouldInvalidate(It.IsAny<CacheValue<string>>())).Returns(false);
+
+            var cache = new DotCache<string, string>(_dummyValueFactory.Object, invalidationStrategyMock.Object,
+                timeProvider);
+
+            var results = StartThreads(threadCount, () => cache.GetOrNull(DummyKey).Result);
+            _dummyValueFactory.Verify(vf => vf.Get(DummyKey), Times.Once);
+        }
+
+        [Theory]
+        [MemberData(nameof(ThreadCounts))]
+        public void GetOrNull_MultipleThreadsAndValueDoesNotExist_AllThreadsReturnRefreshedValue(int threadCount)
+        {
+            var timeProvider = CreateIncrementalTimeProvider();
+            var invalidationStrategyMock = new Mock<IInvalidationStrategy<string>>();
+            invalidationStrategyMock
+                .Setup(x => x.ShouldInvalidate(It.IsAny<CacheValue<string>>())).Returns(false);
+
+            var cache = new DotCache<string, string>(_dummyValueFactory.Object, invalidationStrategyMock.Object,
+                timeProvider);
+            var value = Guid.NewGuid().ToString();
+            _dummyValueFactory
+                .Setup(x => x.Get(DummyKey))
+                .ReturnsAsync(value);
+
+            var results = StartThreads(threadCount, () => cache.GetOrNull(DummyKey).Result);
+
+            results.Count().Should().Be(threadCount);
+            results.Distinct().Should().BeEquivalentTo(value);
+        }
+
+        [Theory]
+        [MemberData(nameof(ThreadCounts))]
+        public void GetOrNull_ValueFactoryThrowsException_AllThreadsReturnNull_1(int threadCount)
+        {
+            var invalidationStrategyMock = new Mock<IInvalidationStrategy<string>>();
+            invalidationStrategyMock
+                .Setup(x => x.ShouldInvalidate(It.IsAny<CacheValue<string>>())).Returns(true);
+
+            var cache = new DotCache<string, string>(_dummyValueFactory.Object, invalidationStrategyMock.Object);
+            _dummyValueFactory
+                .Setup(x => x.Get(DummyKey))
+                .ThrowsAsync(new Exception());
+
+            var results = StartThreads(threadCount, () => cache.GetOrNull(DummyKey).Result);
+
+            results.Count().Should().Be(threadCount);
+            results.Distinct().Should().BeEquivalentTo(new string[] {null});
+        }
+
+        [Theory]
+        [MemberData(nameof(ThreadCounts))]
+        public void GetOrNull_ValueFactoryThrowsException_AllThreadsReturnNull_2(int threadCount)
+        {
+            var invalidationStrategyMock = new Mock<IInvalidationStrategy<string>>();
+            invalidationStrategyMock
+                .Setup(x => x.ShouldInvalidate(It.IsAny<CacheValue<string>>())).Returns(true);
+
+            var cache = new DotCache<string, string>(_dummyValueFactory.Object,
+                invalidationStrategyMock.Object);
+            _dummyValueFactory
+                .SetupSequence(x => x.Get(DummyKey))
+                .ReturnsAsync(DummyKey)
+                .ThrowsAsync(new Exception());
+
+            StartThreads(threadCount, () => cache.GetOrNull(DummyKey).Result);
+            var results = StartThreads(threadCount, () => cache.GetOrNull(DummyKey).Result);
+
+            results.Count().Should().Be(threadCount);
+            results.Distinct().Should().BeEquivalentTo(new string[] {null});
+        }
+        
+        [Fact]
+        public async Task GetOrNull_KeyNotPresentInCache_ValueFactoryGetsCalled()
+        {  
+            var invalidationStrategyMock = new Mock<IInvalidationStrategy<string>>();
+            var cache = new DotCache<string, string>(_dummyValueFactory.Object, invalidationStrategyMock.Object);
+
+            var result = await cache.GetOrNull(DummyKey);
+            result.Should().Be(DummyKey);
+            _dummyValueFactory.Verify(vf => vf.Get(DummyKey), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetOrNull_ValueHasExpired_NewValueIsCreated()
+        {  
+            var invalidationStrategyMock = new Mock<IInvalidationStrategy<string>>();
+            invalidationStrategyMock
+                .Setup(x => x.ShouldInvalidate(It.IsAny<CacheValue<string>>())).Returns(true);
+
+            var cache = new DotCache<string, string>(_dummyValueFactory.Object, invalidationStrategyMock.Object);
+           
+            // to populate value
+            await cache.GetOrNull(DummyKey);
+            
+            // simulate it's stale by now
+            var result = await cache.GetOrNull(DummyKey);
+            
+            result.Should().Be(DummyKey);
+            _dummyValueFactory.Verify(vf => vf.Get(DummyKey), Times.Exactly(2));
+        }
+        
+        [Fact]
+        public async Task GetOrNull_ValueNotExpired_FactoryIsNotCalled()
+        {  
+            var invalidationStrategyMock = new Mock<IInvalidationStrategy<string>>();
+            invalidationStrategyMock
+                .Setup(x => x.ShouldInvalidate(It.IsAny<CacheValue<string>>())).Returns(false);
+
+            var cache = new DotCache<string, string>(_dummyValueFactory.Object, invalidationStrategyMock.Object);
+           
+            // to populate value
+            await cache.GetOrNull(DummyKey);
+            
+            // simulate it's stale by now
+            var result = await cache.GetOrNull(DummyKey);
+            
+            result.Should().Be(DummyKey);
+            _dummyValueFactory.Verify(vf => vf.Get(DummyKey), Times.Once);
+        }
+
+        private static ConcurrentBag<TOut> StartThreads<TOut>(
+            int threadCount,
+            Func<TOut> action)
+        {
+            var resetEvent = new ManualResetEventSlim();
+            var results = new ConcurrentBag<TOut>();
+
+            var threads = Enumerable.Range(0, threadCount).Select(i =>
+            {
+                var t = new Thread(() =>
+                {
+                    resetEvent.Wait();
+                    results.Add(action());
+                }) {Name = $"Test Thread No. {i}"};
+                t.Start();
+                return t;
+            }).ToList();
+
+            resetEvent.Set();
+            foreach (var thread in threads) thread.Join();
+
+            return results;
+        }
+    }
+}
---
+++ DotCached.Core/DotCache.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using DotCached.Core.Infrastructure;
+using DotCached.Core.Logging;
+
+[assembly: InternalsVisibleTo("DotCached.Core.UnitTests")]
+
+namespace DotCached.Core
+{
+    public class DotCache<TKey, TValue>
+        where TValue : class
+    {
+        private static readonly ILog Logger = LogProvider.For<DotCache<TKey, TValue>>();
+
+        private readonly ConcurrentDictionary<TKey, WriteableCacheValue<TValue>> _cache =
+            new ConcurrentDictionary<TKey, WriteableCacheValue<TValue>>();
+
+        private readonly IInvalidationStrategy<TValue> _invalidationStrategy;
+
+        private readonly TimeProvider _timeProvider;
+
+        private readonly IValueFactory<TKey, TValue> _valueFactory;
+
+        public DotCache(IValueFactory<TKey, TValue> valueFactory, IInvalidationStrategy<TValue> invalidationStrategy)
+            : this(valueFactory, invalidationStrategy, TimeProviders.Default)
+        {
+        }
+
+        internal DotCache(IValueFactory<TKey, TValue> valueFactory, IInvalidationStrategy<TValue> invalidationStrategy,
+            TimeProvider timeProvider)
+        {
+            _valueFactory = valueFactory;
+            _invalidationStrategy = invalidationStrategy;
+            _timeProvider = timeProvider;
+        }
+
+        public async Task<TValue> GetOrNull(TKey key)
+        {
+            var expiringValue =
+                _cache.GetOrAdd(key, k => new WriteableCacheValue<TValue>(null, DateTimeOffset.MinValue));
+            if (expiringValue.Value != null && !_invalidationStrategy.ShouldInvalidate(expiringValue))
+                return expiringValue.Value;
+
+            await expiringValue.WriterSemaphore.WaitAsync();
+            try
+            {
+                var currentValue = _cache[key];
+                if (currentValue.Value == null ||
+                    _invalidationStrategy.ShouldInvalidate(currentValue))
+                {
+                    Logger.DebugFormat("Value for key {key} is stale, refreshing..", key);
+                    Set(key, await _valueFactory.Get(key));
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.ErrorException($"Exception while refreshing value for key {key}", ex);
+                Set(key, null);
+            }
+            finally
+            {
+                expiringValue.WriterSemaphore.Release();
+            }
+
+            return _cache[key].Value;
+        }
+
+        public void Set(TKey key, TValue value)
+        {
+            var expiringValue = new WriteableCacheValue<TValue>(value, _timeProvider());
+            _cache.AddOrUpdate(key, expiringValue, (_, __) => expiringValue);
+        }
+    }
+
+    public class CacheValue<TValue>
+    {
+        public CacheValue(TValue value, DateTimeOffset created)
+        {
+            Value = value;
+            Created = created;
+        }
+
+        public TValue Value { get; }
+        public DateTimeOffset Created { get; }
+    }
+
+    internal class WriteableCacheValue<TValue> : CacheValue<TValue>
+    {
+        public WriteableCacheValue(TValue value, DateTimeOffset created)
+            : base(value, created)
+        {
+            WriterSemaphore = new SemaphoreSlim(1, 1);
+        }
+
+        public SemaphoreSlim WriterSemaphore { get; }
+    }
+
+    public interface IInvalidationStrategy<TValue>
+    {
+        bool ShouldInvalidate(CacheValue<TValue> expiringValue);
+    }
+}
